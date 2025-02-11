0dfc1eabbdecf52eeb67f2632c3b4bef4b571be4
Utkarsh Sinha <sinhanikhil366@gmail.com>
2025-02-11 14:49:34 +0530

    Major Changes to the site, Added the full  stack progess page, added some js to make site better, added dsa page

---
+++ script.js
@@ -0,0 +1,11 @@
+const clickableDiv = document.getElementById('clickableDiv');
+
+clickableDiv.addEventListener("click", function(){
+    window.location.href = "FullStack.html";
+});
+
+const clickableDiv2 = document.getElementById('clickableDiv2');
+
+clickableDiv2.addEventListener("click", function(){
+    window.location.href = "dataStructures.html";
+});
